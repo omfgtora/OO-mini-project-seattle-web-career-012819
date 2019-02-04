a8bbde5a95298cc1f79bc83cb3820900f660c91f
omfgtora <spaminol@gmail.com>
2019-02-04 10:19:00 -0800

    RecipeCard scaffolding

--- app/models/RecipeCard.rb
+++ app/models/RecipeCard.rb
@@ -1,7 +1,8 @@
 class RecipeCard
     @@all = []
 
-    attr_reader :name
+    attr_reader :date, :recipe, :user
+    attr_accessor :rating
 
     def initialize(recipe, user)
         @date = Date.today.strftime('%Y-%m-%d') #=> "2019-02-04"
@@ -14,5 +15,4 @@ class RecipeCard
     def self.all
         @@all
     end
-
 end
