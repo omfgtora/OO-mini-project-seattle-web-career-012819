b82db94869c8864ceb7170c5e958f9ad5d313ddd
omfgtora <spaminol@gmail.com>
2019-02-04 09:48:03 -0800

    Recipe method scaffolding

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -5,11 +5,31 @@ class Recipe
 
     def initialize(name)
         @@name = name
-        
+
         @@all << self
     end
 
     def self.all
         @@all
+    end
+
+    def self.most_popular
+        # should return the recipe instance with the highest number of users (the recipe that has the most recipe cards)
+    end
+
+    def users
+        # should return the user instances who have recipe cards with this recipe
+    end
+
+    def ingredients
+        # should return all of the ingredients in this recipe
+    end
+
+    def allergens
+        # should return all of the ingredients in this recipe that are allergens
+    end
+
+    def add_ingredients
+        # should take an array of ingredient instances as an argument, and associate each of those ingredients with this recipe
     end
 end
